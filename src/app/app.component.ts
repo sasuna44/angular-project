5acc95fa30d45ca7cceb3acdadd7d58804499db9
sasuna44 <saramagdi44@std.mans.edu.eg>
2024-05-22 22:11:14 +0300

    home-added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './User/home/home.component';
+import { HomeComponent } from './components/User/home/home.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent ],
+  imports: [RouterOutlet,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
