3cd8fe5c2d622a40e9f7157a56e3c1508ce7b2b3
sasuna44 <saramagdi44@std.mans.edu.eg>
2024-05-23 22:00:27 +0300

    initial-repo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/User/home/home.component'
+import { ProductDetailComponent } from './components/User/product-detail/product-detail.component'
+export const routes: Routes = [
+    // loign-register-routes
 
-export const routes: Routes = [];
+    // user-routes
+    {path:"home",component:HomeComponent},
+    {path: 'product/detail/:id',component:ProductDetailComponent },
+    // this is for the  route
+    {path:'' , redirectTo:'home', pathMatch:'full'},
+    // Admin-routes     
+
+
+];
--- src/app/components/User/products/products.component.ts
+++ src/app/components/User/products/products.component.ts
@@ -1,16 +1,19 @@
 import { AfterViewInit, Component } from '@angular/core';
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-
 import Swiper from 'swiper';
+import { Router, RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements AfterViewInit {
+  constructor(private router: Router) {}
+
   products = [
     {
       id: 1,
@@ -39,29 +42,34 @@ export class ProductsComponent implements AfterViewInit {
   ];
 
   ngAfterViewInit(): void {
-    const productsSwiper = new Swiper('.products-swiper', {
-      pagination: {
-        el: '.swiper-pagination',
-      },
-      autoplay: {
-        delay: 1000,
-        disableOnInteraction: false,
-      },
-    });
+    // const productsSwiper = new Swiper('.products-swiper', {
+    //   pagination: {
+    //     el: '.swiper-pagination',
+    //   },
+    //   autoplay: {
+    //     delay: 1000,
+    //     disableOnInteraction: false,
+    //   },
+    // });
 
-    const startBigDealSwiperAutoplay = () => {
-      productsSwiper.autoplay.start();
-    };
+    // const startBigDealSwiperAutoplay = () => {
+    //   productsSwiper.autoplay.start();
+    // };
 
-    const stopBigDealSwiperAutoplay = () => {
-      productsSwiper.autoplay.stop();
-    };
+    // const stopBigDealSwiperAutoplay = () => {
+    //   productsSwiper.autoplay.stop();
+    // };
 
-    const productsSwiperContainers = document.querySelectorAll('.products-swiper');
-    productsSwiperContainers.forEach(container => {
-      container.addEventListener('mouseenter', startBigDealSwiperAutoplay);
-      container.addEventListener('mouseleave', stopBigDealSwiperAutoplay);
-    });
+    // const productsSwiperContainers = document.querySelectorAll('.products-swiper');
+    // productsSwiperContainers.forEach(container => {
+    //   container.addEventListener('mouseenter', startBigDealSwiperAutoplay);
+    //   container.addEventListener('mouseleave', stopBigDealSwiperAutoplay);
+    // });
   }
+
   faHeart = faHeart;
+
+  goToProduct(id:number): void {
+    this.router.navigate(['product/detail/',id]);
+  }
 }
