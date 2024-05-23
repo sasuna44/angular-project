3baa54043d94f8ceb1a5d891dccff9817d1668e2
yaraGamal7 <yara.gamalll21@gmail.com>
2024-05-23 22:17:11 +0300

    admin dashboard updated

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { APP_ROUTES } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+bootstrapApplication(AppComponent, {
+  providers: [APP_ROUTES, provideHttpClient()]
+}).catch(err => console.error(err));
